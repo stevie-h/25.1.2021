16bb64d7af7e19ec5830504505ad94ac7511c7e6
Stevie H <74920914+stevie-h@users.noreply.github.com>
2021-01-28 14:33:41 +0200

    Create stevieh_hw25012021.java

---
+++ stevieh_hw25012021.java
@@ -0,0 +1,49 @@
+// https://github.com/qa191120/25.01.2021/blob/main/homework.txt
+// class Country
+
+package com.company;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+
+public class Country
+{
+    public String name;
+    public double size;
+    public int numberOfCitizens;
+    public String continent;
+
+/*    public Country(String name, double size, int numberOfCitizens, String continent)
+    {
+        this.name = name;
+        this.size = size;
+        this.numberOfCitizens = numberOfCitizens;
+        this.continent = continent;
+    }
+
+    @Override
+    public String toString()
+    {
+         return "Country{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                ", numberOfCitizens=" + numberOfCitizens +
+                ", continent='" + continent + '\'' +
+                '}';
+    }
+*/
+    public void printCountrySize()
+    {
+
+    }
+
+    public void printSizePerCitizen()
+    {
+
+    }
+
+    
+}
